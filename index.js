12742334e0150d21a5afedf5a3f2c0d13be2324e
Deepanshu <deepanshu.pal.17@mountblue.tech>
2021-09-17 14:23:48 +0530

    Completed GET/html GET/json using express

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const path = require('path');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
 
+app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/html', (req, res) => {
+    // res.sendFile(path.join(__dirname, '/index.html'));
+    res.redirect("/index.html");
+});
+
+app.get('/json', (req, res) => {
+    // res.sendFile(path.join(__dirname, '/json.html'));
+    res.redirect("/data.json");
+});
 
 app.listen(8080, () => {
     console.log("Server is Running");
